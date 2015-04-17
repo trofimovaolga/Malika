cc8847fd424b411d015171ceefc5f8c8d78b66a9
trofimovaolga <trofimova.olya@mail.ru>
2015-04-18 01:27:03 +1000

    Added own catching of program errors, renamed button

--- forthepoor.pas
+++ forthepoor.pas
@@ -23,6 +23,7 @@ type
     procedure MenuAboutClick(Sender: TObject);
     procedure MenuExitClick(Sender: TObject);
     procedure MenuItemClick(Sender: TObject);
+    procedure Exception(Sender: TObject; E: Exception);
   end;
 
 var
@@ -49,11 +50,18 @@ begin
     end;
     Reference.Add(NewItem);
   end;
+  Application.OnException := @Exception;
 end;
 
 procedure TMainForm.MenuItemClick(Sender: TObject);
 begin
   MakeForm((Sender as TMenuItem).tag);
+end;
+
+procedure TMainForm.Exception(Sender: TObject; E: Exception);
+begin
+  ShowOnTop;
+  Memo.Text := E.Message;
 end;
 
 procedure TMainForm.MenuAboutClick(Sender: TObject);
